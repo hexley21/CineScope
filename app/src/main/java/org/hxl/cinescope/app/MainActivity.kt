330f0e700589b3a18dd0490f67b920664353341e
Hexley21 <a.kupreishvili.21@gmail.com>
2023-06-17 19:33:13 +0400

    feat: add splash-screen

--- app/src/main/java/org/hxl/cinescope/app/MainActivity.kt
+++ app/src/main/java/org/hxl/cinescope/app/MainActivity.kt
@@ -1,12 +1,14 @@
 package org.hxl.cinescope.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import org.hxl.cinescope.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().setKeepOnScreenCondition { false }
         setContentView(R.layout.activity_main)
     }
 }
